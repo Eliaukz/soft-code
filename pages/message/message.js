c8aa35f85737f39f715ed6038d175f54a74f8e21
xiao-chuachua <U202115586@hust.edu.cn>
2023-10-31 16:55:24 +0800

    first

--- pages/message/message.js
+++ pages/message/message.js
@@ -1,78 +1,76 @@
-const app = getApp()
-
+const app = getApp();
+const db = wx.cloud.database();
 Page({
-    data: {
-        userInfo: null,
-        my_friends:[]
-    },
+  data: {
+    my_friends: [],
+  },
 
-    onLoad() {
-        
-        this.setData({
-            userInfo : app.globalData.userInfo
-        })
-    },
-    onShow() {
-        
-        this.setData({
-            userInfo : app.globalData.userInfo,
-            my_friends : []
-        })
-        this.loadUser()
-        this.getMyfriend()
-    },
-    loadUser() {
-        var that = this;
-        wx.cloud.database().collection('user').where({
-            account_id : that.data.userInfo.account_id,
-            password: that.data.userInfo.password
-        }).get({
-            success(res) {
-                console.log(res)
-                // 更新数据 拿到 _id
-                app.globalData.userInfo = res.data[0]
-                that.setData({
-                    userInfo: app.globalData.userInfo
-                })
-            }
-        })
-    },
+  onLoad() {
+    this.setData({
+      userInfo: app.globalData.userInfo,
+    });
+  },
+  onShow() {
+    this.setData({
+      userInfo: app.globalData.userInfo,
+      my_friends: [],
+    });
+    this.loadUser();
+    this.getMyfriend();
+  },
 
+  loadUser() {
+    var that = this;
+    db.collection("user")
+      .where({
+        account_id: that.data.userInfo.account_id,
+        password: that.data.userInfo.password,
+      })
+      .get({
+        success(res) {
+          console.log(res);
+          // 更新数据 拿到 _id
+          app.globalData.userInfo = res.data[0];
+          that.setData({
+            userInfo: app.globalData.userInfo,
+          });
+        },
+      });
+  },
 
-    getMyfriend() {
-        // 获取所有成功添加好友的朋友
-        var that = this;
-        const DB = wx.cloud.database().command;
-        wx.cloud.database().collection('chat_record').where(
-            DB.or([
-                {
-                    userA_id: app.globalData.userInfo._id,
-                    friend_status: true
-                },
-                {
-                    userB_id: app.globalData.userInfo._id,
-                    friend_status: true
-                }
-            ])
-        ).watch({
-            onChange: function(snapshot){
-                that.setData({
-                    my_friends : snapshot.docs
-                })
-            },
-            onError : function(err){
-                console.log(err)
-            }
-        })
-    },
+  getMyfriend() {
+    // 获取所有成功添加好友的朋友
+    var that = this;
+    const DB = wx.cloud.database().command;
+    db.collection("chat_record")
+      .where(
+        DB.or([
+          {
+            userA_id: app.globalData.userInfo._id,
+            friend_status: true,
+          },
+          {
+            userB_id: app.globalData.userInfo._id,
+            friend_status: true,
+          },
+        ])
+      )
+      .watch({
+        onChange: function (snapshot) {
+          that.setData({
+            my_friends: snapshot.docs,
+          });
+        },
+        onError: function (err) {
+          console.log(err);
+        },
+      });
+  },
 
-    startChat(e) {
-
-        var index = e.currentTarget.dataset.index;
-        console.log(this.data.my_friends)
-        wx.navigateTo({
-          url: '/pages/chat/chat?id=' + this.data.my_friends[index]._id
-        })
-        
-    }
-})
+  startChat(e) {
+    var index = e.currentTarget.dataset.index;
+    wx.navigateTo({
+      url: "/pages/chat/chat?id=" + this.data.my_friends[index]._id,
+    });
+  },
+});

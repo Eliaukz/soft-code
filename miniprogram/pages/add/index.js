29c1afd0304a6ee36de3d48631882747aa1824b5
xiao-chuachua <U202115586@hust.edu.cn>
2023-11-03 21:17:18 +0800

    ...

--- miniprogram/pages/add/index.js
+++ miniprogram/pages/add/index.js
@@ -1,30 +1,35 @@
 /* 新增待办页面 */
 const app = getApp();
-Page({           
+Page({
   // 保存编辑中待办的
   data: {
-    owner: app.globalData.userInfo,
-    title: '',
-    desc: '',
+    owner: null,
+    title: "",
+    desc: "",
     price: 0,
     files: [],
-    fileName: '',
+    fileName: "",
     freq: 0,
-    addressOptions: ['沁苑', '紫菘', '韵苑'],
-    address: 0
+    addressOptions: ["沁苑", "紫菘", "韵苑"],
+    address: 0,
   },
 
+  onLoad() {
+    this.setData({
+      owner: app.globalData.userInfo,
+    });
+  },
   // 表单输入处理函数
   onTitleInput(e) {
     this.setData({
-      title: e.detail.value
-    })
+      title: e.detail.value,
+    });
   },
 
   onDescInput(e) {
     this.setData({
-      desc: e.detail.value
-    })
+      desc: e.detail.value,
+    });
   },
 
   // 上传新附件
@@ -32,102 +37,108 @@ Page({
     // 限制附件个数
     if (this.data.files.length + 1 > getApp().globalData.fileLimit) {
       wx.showToast({
-        title: '数量达到上限',
-        icon: 'error',
-        duration: 2000
-      })
-      return
+        title: "数量达到上限",
+        icon: "error",
+        duration: 2000,
+      });
+      return;
     }
     // 从会话选择文件
     wx.chooseMessageFile({
-      count: 1
-    }).then(res => {
-      const file = res.tempFiles[0]
+      count: 1,
+    }).then((res) => {
+      const file = res.tempFiles[0];
       // 上传文件至云存储
-      getApp().uploadFile(file.name,file.path).then(res => {
-        // 追加文件记录，保存其文件名、大小和文件 id
-        this.data.files.push({
-          name: file.name,
-          size: (file.size / 1024 / 1024).toFixed(2),
-          id: res.fileID
-        })
-        // 更新文件显示
-        this.setData({
-          files: this.data.files,
-          fileName: this.data.fileName + file.name + ' '
-        })
-      })
-    })
+      getApp()
+        .uploadFile(file.name, file.path)
+        .then((res) => {
+          // 追加文件记录，保存其文件名、大小和文件 id
+          this.data.files.push({
+            name: file.name,
+            size: (file.size / 1024 / 1024).toFixed(2),
+            id: res.fileID,
+          });
+          // 更新文件显示
+          this.setData({
+            files: this.data.files,
+            fileName: this.data.fileName + file.name + " ",
+          });
+        });
+    });
   },
-  onPriceInput(e){
+  onPriceInput(e) {
     this.setData({
-      price: e.detail.value
-    })
+      price: e.detail.value,
+    });
   },
   // 响应事件状态选择器
   onChooseAddress(e) {
     this.setData({
-      freq: e.detail.value
-    })
+      freq: e.detail.value,
+    });
   },
 
   // 保存待办
   async saveTodo() {
     // 对输入框内容进行校验
-    if (this.data.title === '') {
+    if (this.data.title === "") {
       wx.showToast({
-        title: '事项标题未填写',
-        icon: 'error',
-        duration: 2000
-      })
-      return
+        title: "事项标题未填写",
+        icon: "error",
+        duration: 2000,
+      });
+      return;
     }
     if (this.data.title.length > 10) {
       wx.showToast({
-        title: '事项标题过长',
-        icon: 'error',
-        duration: 2000
-      })
-      return
+        title: "事项标题过长",
+        icon: "error",
+        duration: 2000,
+      });
+      return;
     }
     if (this.data.desc.length > 100) {
       wx.showToast({
-        title: '事项描述过长',
-        icon: 'error',
-        duration: 2000
-      })
-      return
+        title: "事项描述过长",
+        icon: "error",
+        duration: 2000,
+      });
+      return;
     }
-    const db = await getApp().database()
+    const db = await getApp().database();
     // 在数据库中新建待办事项，并填入已编辑对信息
-    db.collection(getApp().globalData.collection).add({
-      data: {
-        price: Number(this.data.price),// 用户信息
-        title: this.data.title,       // 书名
-        desc: this.data.desc,         // 描述
-        files: this.data.files,       // 附件列表
-        address: Number(this.data.address), // 地址
-        freq:0,                         // 是否上架
-        star: false   
-      }
-    }).then(() => {
-      wx.navigateBack({
-        delta: 0,
+    console.log(this.data.owner);
+    db.collection(getApp().globalData.collection)
+      .add({
+        data: {
+          price: Number(this.data.price), // 用户信息
+          title: this.data.title, // 书名
+          desc: this.data.desc, // 描述
+          files: this.data.files, // 附件列表
+          address: Number(this.data.address), // 地址
+          freq: 0, // 是否上架
+          star: false,
+          owner: this.data.owner._id,
+        },
       })
-    })
+      .then(() => {
+        wx.navigateBack({
+          delta: 0,
+        });
+      });
   },
 
   // 重置所有表单项
   resetTodo() {
     this.setData({
-      title: '',
-      desc: '',
+      title: "",
+      desc: "",
       price: 0,
       files: [],
-      fileName: '',
-      addressOptions: ['沁苑', '紫菘', '韵苑'],
+      fileName: "",
+      addressOptions: ["沁苑", "紫菘", "韵苑"],
       address: 0,
-      freq: 0
-    })
-  }
-})
+      freq: 0,
+    });
+  },
+});
--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -1,66 +1,63 @@
-
-const app = getApp()
+const app = getApp();
 
 Page({
-  data: {
-      
-  },
+  data: {},
   onLoad() {
-    console.log(app.globalData.userInfo)
-    if (app.globalData.userInfo!=null){
+    console.log(app.globalData.userInfo);
+    if (app.globalData.userInfo != null) {
       wx.switchTab({
-        url: '/pages/message/message',
-      })
+        url: "/pages/message/message",
+      });
     }
   },
-  
+
   getUserAccount(e) {
     this.setData({
-      account_id : e.detail.value
-    })
+      account_id: e.detail.value,
+    });
   },
   getUserPassword(e) {
     this.setData({
-      password : e.detail.value
-    })
+      password: e.detail.value,
+    });
   },
   login() {
-      var that = this;
-      wx.cloud.database().collection('user').where({
+    var that = this;
+    wx.cloud
+      .database()
+      .collection("user")
+      .where({
         account_id: that.data.account_id,
-        password: that.data.password
-      }).get({
-          success(res) {
-              console.log(res)
-              if(res.data.length>0){
-                  // 拿到 _id
-                  app.globalData.userInfo = res.data[0]
-                  wx.setStorageSync('userInfo', res.data[0])
-                  wx.switchTab({
-                    url: '/pages/message/message',
-                    success(res){
-                        wx.showToast({
-                          title: '登陆成功',
-                        })
-                    }
-                  })
-              } else {
-                  wx.showToast({
-                    icon: 'none',
-                    title: '账号密码错误',
-                  })
-              }
-          }
-          
+        password: that.data.password,
       })
+      .get({
+        success(res) {
+          console.log(res);
+          if (res.data.length > 0) {
+            // 拿到 _id
+            app.globalData.userInfo = res.data[0];
+            wx.setStorageSync("userInfo", res.data[0]);
+            wx.switchTab({
+              url: "/pages/message/message",
+              success(res) {
+                wx.showToast({
+                  title: "登陆成功",
+                });
+              },
+            });
+          } else {
+            wx.showToast({
+              icon: "none",
+              title: "账号密码错误",
+            });
+          }
+        },
+      });
   },
-
 
   toRegister() {
-      wx.navigateTo({
-        url: '/pages/register/register',
-      })
+    wx.navigateTo({
+      url: "/pages/register/register",
+    });
   },
-
-  
-})
+});
